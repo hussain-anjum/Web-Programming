cdc9ec10dfc723a899c0c9bf3ed2d33ba93a589c
hussain-anjum <haratul14@gmail.com>
2026-02-21 13:11:39 +0600

    Fix formatting of welcome message in admin panel

--- AdminPanel/admin.php
+++ AdminPanel/admin.php
@@ -9,13 +9,11 @@ if (!isset($_SESSION['username'])) {
 <html>
 <head><title>Admin Panel</title></head>
 <body>
-<h1>Welcome, <?= htmlspecialchars($_SESSION['username']) ?>!</h1>
-
-<ul>
-    <li><a href="security.php">Security Page</a></li>
-    <li><a href="logout.php">Logout</a></li>
-</ul>
-
-<p>This is the admin panel demo page.</p>
+    <h1>Welcome, <?= $_SESSION['username'] ?>!</h1>
+    <ul>
+        <li><a href="security.php">Security Page</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <p>This is the admin panel demo page.</p>
 </body>
 </html>
